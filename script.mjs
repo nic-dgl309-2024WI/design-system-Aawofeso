f80d2d1797b48183f10e4d0f0727d1494d0884cf
Aawofeso <144481917+Aawofeso@users.noreply.github.com>
2024-02-08 17:29:01 +0100

    Format hamburger icon to show side navbar

--- script.mjs
+++ script.mjs
@@ -1,9 +1,15 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
+ 
   /* Set the width of the side navigation to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  function myFunction() {
+    var x = document.getElementById("mySidenav").style.width = "250px";
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
