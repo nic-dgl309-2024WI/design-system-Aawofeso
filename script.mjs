b69b067dc28adcfdb9322f5fc4f6933f0a656dbf
Aawofeso <144481917+Aawofeso@users.noreply.github.com>
2024-02-08 17:55:42 +0100

    Format topnav to hide all links in mobile version

--- script.mjs
+++ script.mjs
@@ -1,4 +1,4 @@
- 
+  
   /* Set the width of the side navigation to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
@@ -6,10 +6,5 @@
 
   function myFunction() {
     var x = document.getElementById("mySidenav").style.width = "250px";
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
   }
   
