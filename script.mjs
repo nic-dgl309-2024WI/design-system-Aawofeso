4b8a7f711c053c34b4e547e32c46cda0f04124aa
Aawofeso <144481917+Aawofeso@users.noreply.github.com>
2024-02-12 22:02:49 +0100

    Made the side bar push content to the right when opened

--- script.mjs
+++ script.mjs
@@ -2,9 +2,12 @@
   /* Set the width of the side navigation to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
   }
 
   function myFunction() {
     var x = document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
   }
+
   
