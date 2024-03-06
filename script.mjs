88b0d16853a320968597b73f4642c67bb5d3d75a
Aawofeso <144481917+Aawofeso@users.noreply.github.com>
2024-03-06 15:50:20 +0100

    change the line-height of the body tag

--- script.mjs
+++ script.mjs
@@ -1,13 +1,38 @@
   
   /* Set the width of the side navigation to 0 */
+
+
+
+
+
   function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    x.style.width = "0"; 
+    y.style.marginLeft = "0";
   }
 
   function myFunction() {
-    var x = document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    
+   x.style.width = "250px"; 
+   y.style.marginLeft = "250px";
   }
+
+
+  function resize() {
+
+    if (z.style.display == 'none') { 
+      z.style.display = 'block';
+    } else {
+      z.style.display = 'none';
+    }
+
+  }
+
+
+  z.addEventListener('click', resize);
+
+  const x = document.getElementById("mySidenav");
+  const y = document.getElementById("main");
+  const z = document.getElementById("icon");
+ 
 
   
